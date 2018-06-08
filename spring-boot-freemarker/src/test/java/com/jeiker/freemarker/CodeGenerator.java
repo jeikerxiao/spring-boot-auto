9e2b16f381877410d944c29d96cbc4940144dbf5
jeikerxiao <jeiker@126.com>
2018-06-08 10:00:15 +0800

    init project

---
+++ spring-boot-freemarker/src/test/java/com/jeiker/freemarker/CodeGenerator.java
@@ -0,0 +1,107 @@
+package com.jeiker.freemarker;
+
+import com.google.common.base.CaseFormat;
+import freemarker.template.TemplateExceptionHandler;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static com.jeiker.freemarker.ProjectConstant.*;
+
+/**
+ * Description: spring-boot-freemarker
+ * Created by jeikerxiao on 2018/6/8 上午9:47
+ */
+public class CodeGenerator {
+
+    private static final String PROJECT_PATH = System.getProperty("user.dir");//项目在硬盘上的基础路径
+    private static final String TEMPLATE_FILE_PATH = PROJECT_PATH + "/src/test/resources/generator/template";//模板位置
+
+    private static final String JAVA_PATH = "/src/main/java"; //java文件路径
+    private static final String RESOURCES_PATH = "/src/main/resources";//资源文件路径
+
+    private static final String PACKAGE_PATH_SERVICE = packageConvertPath(SERVICE_PACKAGE);//生成的Service存放路径
+    private static final String PACKAGE_PATH_SERVICE_IMPL = packageConvertPath(SERVICE_IMPL_PACKAGE);//生成的Service实现存放路径
+    private static final String PACKAGE_PATH_CONTROLLER = packageConvertPath(CONTROLLER_PACKAGE);//生成的Controller存放路径
+
+    private static final String AUTHOR = "jeikerxiao";//@author
+    private static final String DATE = new SimpleDateFormat("yyyy/MM/dd").format(new Date());//@date
+
+
+    public static void main(String[] args) {
+        genCode("user");
+//        genCode("输入表名");
+        //genCodeByCustomModelName("输入表名","输入自定义Model名称");
+    }
+
+    /**
+     * 通过数据表名称生成代码，Model 名称通过解析数据表名称获得，下划线转大驼峰的形式。
+     * 如输入表名称 "t_user_detail" 将生成 TUserDetail、TUserDetailMapper、TUserDetailService ...
+     *
+     * @param tableNames 数据表名称...
+     */
+    public static void genCode(String... tableNames) {
+        for (String tableName : tableNames) {
+            genCodeByCustomModelName(tableName, null);
+        }
+    }
+
+    /**
+     * 通过数据表名称，和自定义的 Model 名称生成代码
+     * 如输入表名称 "t_user_detail" 和自定义的 Model 名称 "User" 将生成 User、UserMapper、UserService ...
+     *
+     * @param tableName 数据表名称
+     * @param modelName 自定义的 Model 名称
+     */
+    public static void genCodeByCustomModelName(String tableName, String modelName) {
+        genModelAndMapper(tableName, modelName);
+        genService(tableName, modelName);
+        genController(tableName, modelName);
+    }
+
+    private static void genController(String tableName, String modelName) {
+
+    }
+
+    private static void genService(String tableName, String modelName) {
+
+    }
+
+    private static void genModelAndMapper(String tableName, String modelName) {
+
+    }
+
+
+    private static freemarker.template.Configuration getConfiguration() throws IOException {
+        freemarker.template.Configuration cfg = new freemarker.template.Configuration(freemarker.template.Configuration.VERSION_2_3_23);
+        cfg.setDirectoryForTemplateLoading(new File(TEMPLATE_FILE_PATH));
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.IGNORE_HANDLER);
+        return cfg;
+    }
+
+    private static String tableNameConvertLowerCamel(String tableName) {
+        return CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, tableName.toLowerCase());
+    }
+
+    private static String tableNameConvertUpperCamel(String tableName) {
+        return CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, tableName.toLowerCase());
+
+    }
+
+    private static String tableNameConvertMappingPath(String tableName) {
+        tableName = tableName.toLowerCase();//兼容使用大写的表名
+        return "/" + (tableName.contains("_") ? tableName.replaceAll("_", "/") : tableName);
+    }
+
+    private static String modelNameConvertMappingPath(String modelName) {
+        String tableName = CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, modelName);
+        return tableNameConvertMappingPath(tableName);
+    }
+
+    private static String packageConvertPath(String packageName) {
+        return String.format("/%s/", packageName.contains(".") ? packageName.replaceAll("\\.", "/") : packageName);
+    }
+}
